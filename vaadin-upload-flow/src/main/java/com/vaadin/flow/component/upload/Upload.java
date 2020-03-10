f8645ee13ae1f11493ee5b97b011c1036b44f827
hongduc-phan <phduc.phanhongduc@gmail.com>
2020-03-10 16:56:15 +0200

    refactor the comment to be clearer due to suggestion

--- vaadin-upload-flow/src/main/java/com/vaadin/flow/component/upload/Upload.java
+++ vaadin-upload-flow/src/main/java/com/vaadin/flow/component/upload/Upload.java
@@ -268,7 +268,7 @@ public class Upload extends GeneratedVaadinUpload<Upload> implements HasSize {
      *
      * @param uploadButton
      *            the component to be clicked by the user to open the dialog,
-     *            or <code>null</code> to use vaadin-button as the actionable button
+     *            or <code>null</code> to reset to the default button
      */
     public void setUploadButton(Component uploadButton) {
         removeElementsAtSlot("add-button");
